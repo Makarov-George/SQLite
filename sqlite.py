e79f5438f9a7a93d0ad25a7b633a07b1201b31fd
Макаров Георгий Алексеевич <georgijmak9692@gmail.com>
2024-10-29 21:19:42 +0300

    Составить запросы ждÐдля задания 2

--- sqlite.py
+++ sqlite.py
@@ -5,14 +5,53 @@ try:
     cursor = sqlite_connection.cursor()
     print("База данных создана и успешно подключена к SQLite")
 
-    students = [(1, 'Max', 'Brooks', 24, 'Spb'), (2, 'John', 'Stones', 15, 'Spb'), (3, 'Andy', 'Wings', 45, 'Manhester'), (4, 'Kate', 'Brooks', 34, 'Spb')]
-    courses = [(1, 'python', '21.07.21', '21.08.21'), (2, 'java', '13.07.21', '16.08.21')]
-    students_courses = [(1, 1), (2, 1), (3, 1), (4, 2)]
-    cursor.executemany("INSERT INTO Students VALUES(?, ?, ?, ?, ?)", students)
-    cursor.executemany("INSERT INTO Courses VALUES(?, ?, ?, ?)", courses)
-    cursor.executemany("INSERT INTO Student_courses VALUES(?, ?)", students_courses)
+    def students_older_30(cursor):
+        print('Студенты старше 30 лет:')
+        cursor.execute('''SELECT name, surname from Students where age > 30''')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row[0] + ' ' + row[1])
+
+    def students_python(cursor):
+        print('Студенты обущающиеся на курсе по питону:')
+        cursor.execute('''SELECT id from Courses where name = "python"''')
+        id_course = cursor.fetchall()
+        for i in id_course:
+            id_course = i
+        
+        cursor.execute('''SELECT student_id from Student_courses where course_id = ?''', id_course)
+        id_student = cursor.fetchall()
+        for i in id_student:
+            id_student = i
+            cursor.execute('''SELECT name, surname from Students where id = ?''', id_student)
+            student = cursor.fetchall()
+            for i in student:
+                print(i[0] + ' ' + i[1])
+
+    def students_python_Spb(cursor):
+        print('Студенты обущающиеся на курсе по питону и живущие в Спб:')
+        cursor.execute('''SELECT id from Courses where name = "python"''')
+        id_course = cursor.fetchall()
+        for i in id_course:
+            id_course = i
+        
+        cursor.execute('''SELECT student_id from Student_courses where course_id = ?''', id_course)
+        id_student = cursor.fetchall()
+        for i in id_student:
+            id_student = i
+            cursor.execute('''SELECT name, surname from Students where id = ? and city = "Spb"''', id_student)
+            student = cursor.fetchall()
+            if len(student) != 0:
+                for i in student:
+                    print(i[0] + ' ' + i[1])
+            else:
+                break
+    
+    students_older_30(cursor)
+    students_python(cursor)
+    students_python_Spb(cursor)
     sqlite_connection.commit()
-    print("Таблицы заполнены")
+    print("Выполнено")
     cursor.close()
 
 except _sqlite3.Error as error:
